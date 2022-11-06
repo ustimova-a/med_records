23b1f54616e9074660d8989d3936389ba593daf0
Anna Ustimova <ustimova.a@gmail.com>
2022-11-06 19:43:03 +0300

    added models

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Hospital, Specialty, Physician, Condition, Drug, Document, Treatment
 
-# Register your models here.
+
+class HospitalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'website')
+
+
+class PhysicianAdmin(admin.ModelAdmin):
+    exclude = ('patronymic',)
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('date_issued', 'physician', 'condition', 'prescribed_drug', 'source_doc')
+
+
+class TreatmentAdmin(admin.ModelAdmin):
+    list_display = ('prescribed_drug_list', 'dosage', 'dosing_regimen', 'comment')
+
+    def prescribed_drug_list(self, treatment):
+        return ', '.join(map(str, treatment.prescribed_drug.values_list('id', flat=True)))
+
+
+admin.site.register(Hospital, HospitalAdmin)
+admin.site.register(Specialty)
+admin.site.register(Physician, PhysicianAdmin)
+admin.site.register(Condition)
+admin.site.register(Drug)
+admin.site.register(Document, DocumentAdmin)
+admin.site.register(Treatment, TreatmentAdmin)
+
+
+
--- records/models.py
+++ records/models.py
@@ -1,37 +1,74 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 class Hospital(models.Model):
     name = models.CharField(max_length=255)
-    website = models.URLField()
+    website = models.URLField(default=None)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+    class Meta:
+        verbose_name_plural = 'Specialties'
 
 
 class Physician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     patronymic = models.CharField(max_length=50)
-    specialty = models.CharField(max_length=50)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=12)
+    is_recommended = models.BooleanField(default=False)
     comment = models.TextField()
+
+    def __str__(self):
+        return str(self.last_name)
 
 
 class Condition(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return str(self.name)
+
 
 class Drug(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return str(self.name)
+
 
 class Document(models.Model):
     date_issued = models.DateField()
-    physician = models.ForeignKey(Physician, on_delete=models.PROTECT)
-    condition = models.ForeignKey(Condition, on_delete=models.PROTECT)
-    prescribed_drug = models.ForeignKey(Drug, on_delete=models.PROTECT)
-    # source_doc = models.FileField(upload_to=)
-    # def user_directory_path(instance, filename):
-    # # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
-    # return 'user_{0}/{1}'.format(instance.user.id, filename)
-    # + MEDIA_ROOT in settings.py
+    physician = models.ForeignKey(Physician, on_delete=models.CASCADE)
+    condition = models.ForeignKey(Condition, on_delete=models.CASCADE)
+    prescribed_drug = models.ForeignKey(Drug, on_delete=models.CASCADE)
+    source_doc = models.FileField(upload_to="uploads/%Y/%m/%d/")
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Treatment(models.Model):
+    prescribed_drug = models.ManyToManyField(Drug)
+    dosage = models.FloatField(validators=[MinValueValidator(0)])
+    dosing_regimen = models.CharField(max_length=255)
+    comment = models.TextField()
+
+    def __str__(self):
+        return str(self.id)
+
+
+   
 
     
